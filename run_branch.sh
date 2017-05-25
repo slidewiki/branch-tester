c4fc0a8fc87165f6761c8c6462714d889a14f5ac
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2017-05-25 14:22:24 +0200

    fixed DNS lookup problem in super secure VPNs

--- run_branch.sh
+++ run_branch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name slidewiki-branch-tester -p 3000:3000 -e BRANCH=$1 slidewiki/branch-tester
+docker run --name slidewiki-branch-tester --dns 8.8.8.8 -p 3000:3000 -e BRANCH=$1 slidewiki/branch-tester
