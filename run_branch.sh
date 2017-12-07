a075b0e970d2d0df61ac4111fb40b0e6e5449ac3
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2017-12-07 23:11:36 +0100

    fix2

--- run_branch.sh
+++ run_branch.sh
@@ -4,6 +4,5 @@ if [[ -z "$2" ]]; then
   docker run --name slidewiki-branch-tester --dns 8.8.8.8 -p 3000:3000 -e BRANCH=$1 slidewiki/branch-tester
 else
   echo "NOTICE: Using custom configuration to run platform!"
-  cp $2 config
   docker run --name slidewiki-branch-tester --dns 8.8.8.8 -p 3000:3000 -e BRANCH=$1 -e USE_CUSTOM_MICROSERVICES=1 -v $2:/config/microservices.js slidewiki/branch-tester
 fi
